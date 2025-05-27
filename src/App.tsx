cd081a91eb9d676ed2d0391a18de453ade35ac5f
leehyunha <jewel2503@naver.com>
2025-05-28 05:43:13 +0900

    [chore] 라우팅 경로 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,31 @@
 import MainPage from './pages/Mainpage';
+import SelectRepoPage from './pages/SelectRepoPage';
+import SelectFilePage from './pages/SelectFilePage';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate
+} from 'react-router-dom';
 
 function App() {
-  return <MainPage />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* 홈 화면 */}
+        <Route path="/" element={<MainPage />} />
+
+        {/* http://localhost:5174/select 에 들어오면 SelectRepoPage 렌더링 */}
+        <Route path="/select" element={<SelectRepoPage />} />
+
+        {/* http://localhost:5174/select-file 에 들어오면 SelectFilePage 렌더링 */}
+        <Route path="/select-file" element={<SelectFilePage />} />
+
+        {/* 그 외 모든 경로는 홈으로 리다이렉트 */}
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
