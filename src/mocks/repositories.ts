0209d1980f5393e505d63022c6643d3d0764d598
Lumire-ee <58719969+Lumire-ee@users.noreply.github.com>
2025-05-29 19:27:16 +0900

    [chore] RepositoryList용 mock 데이터 생성

---
+++ src/mocks/repositories.ts
@@ -0,0 +1,52 @@
+import type { Repository } from '../types/repository';
+
+export const dummyData: Repository[] = [
+  { id: 1, name: '1 Repository', language: 'Java', visibility: 'public', updated_at: '2024-05-01' },
+  {
+    id: 2,
+    name: '2 Repository',
+    language: 'Python',
+    visibility: 'private',
+    updated_at: '2024-05-02',
+  },
+  { id: 3, name: '3 Repository', language: 'HTML', visibility: 'public', updated_at: '2024-05-03' },
+  {
+    id: 4,
+    name: '4 Repository',
+    language: 'JavaScript',
+    visibility: 'public',
+    updated_at: '2024-05-04',
+  },
+  { id: 5, name: '5 Repository ', language: 'Go', visibility: 'private', updated_at: '2024-05-05' },
+  { id: 6, name: '6 Repository', language: 'Rust', visibility: 'public', updated_at: '2024-05-06' },
+  { id: 7, name: '7 Repository', language: 'Java', visibility: 'public', updated_at: '2024-05-07' },
+  {
+    id: 8,
+    name: '8 Repository',
+    language: 'Python',
+    visibility: 'private',
+    updated_at: '2024-05-08',
+  },
+  { id: 9, name: '9 Repository', language: 'HTML', visibility: 'public', updated_at: '2024-05-09' },
+  {
+    id: 10,
+    name: '10 Repository',
+    language: 'JavaScript',
+    visibility: 'public',
+    updated_at: '2024-05-10',
+  },
+  {
+    id: 11,
+    name: '11 Repository',
+    language: 'Go',
+    visibility: 'private',
+    updated_at: '2024-05-11',
+  },
+  {
+    id: 12,
+    name: '12 Repository',
+    language: 'Rust',
+    visibility: 'public',
+    updated_at: '2024-05-12',
+  },
+];
