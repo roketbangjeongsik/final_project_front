3e23939d1da23e04496ffece5c8690319125d8ab
Lumire-ee <58719969+Lumire-ee@users.noreply.github.com>
2025-05-30 20:33:26 +0900

    [feat] react-router-dom 적용 및 라우트 기본 설정
    - react-router-dom 의존성 설치
    - 라우팅 추가 및 초기 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
+import { Routes, Route } from 'react-router-dom';
 import MainPage from './pages/Mainpage';
 import { RepositoryPage } from './pages/RepositoryPage';
+import { FilePage } from './pages/FilePage';
 
 function App() {
-  return <RepositoryPage />;
+  return (
+    <Routes>
+      <Route path="/" element={<MainPage />} />
+      <Route path="/repo" element={<RepositoryPage />} />
+      <Route path="/file" element={<FilePage />} />
+    </Routes>
+  );
 }
 
 export default App;
