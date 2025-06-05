6b28af443e1018ace2a4aa4b1afd5711166b861f
Lumire-ee <58719969+Lumire-ee@users.noreply.github.com>
2025-06-05 14:58:33 +0900

    [feat] useUser 훅 추가로 사용자 정보 관리 로직 분리
    - 사용자 정보를 가져오고, 로딩/로그아웃 관리하는 커스텀 훅

---
+++ src/hooks/useUser.ts
@@ -0,0 +1,24 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import type { User } from '../types/user';
+
+export const useUser = () => {
+  const [user, setUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    axios
+      .get<User>('/api/v1/user/me', { withCredentials: true })
+      .then(res => setUser(res.data))
+      .catch(() => setUser(null))
+      .finally(() => setLoading(false));
+  }, []);
+  console.log(user);
+
+  // 로그아웃 API 호출
+  const logout = () => {
+    setUser(null);
+  };
+
+  return { user, loading, logout };
+};
