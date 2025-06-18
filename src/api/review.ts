e81323b529f8505a8fad0de1e599960269995517
Lumire-ee <58719969+Lumire-ee@users.noreply.github.com>
2025-06-18 22:51:52 +0900

    [refactor] isDev분기 제거

--- src/api/review.ts
+++ src/api/review.ts
@@ -1,23 +1,23 @@
 import axios from 'axios';
 
-const isDev = import.meta.env.MODE === 'development';
+// const isDev = import.meta.env.MODE === 'development';
 
-export const fetchCodeReviewResult = async (diff: string): Promise<string> => {
-  if (isDev) {
-    console.log('개발 모드에서는 실제 API 호출을 하지 않습니다.');
-    return '개발 모드에서는 실제 API 호출을 하지 않습니다.';
-  }
+export const fetchCodeReviewResult = async (patch: string): Promise<string> => {
+  // if (isDev) {
+  //   console.log('개발 모드에서는 실제 API 호출을 하지 않습니다.');
+  //   return '개발 모드에서는 실제 API 호출을 하지 않습니다.';
+  // }
 
-  const res = await axios.post('/api/v1/pr/review', { diff });
+  const res = await axios.post('/api/v1/pr/review', { patch });
   return res.data.choices?.[0]?.message?.content ?? '설명 없음';
 };
 
-export const fetchRefactorResult = async (diff: string): Promise<string> => {
-  if (isDev) {
-    console.log('개발 모드에서는 실제 API 호출을 하지 않습니다.');
-    return '개발 모드에서는 실제 API 호출을 하지 않습니다.';
-  }
+export const fetchRefactorResult = async (patch: string): Promise<string> => {
+  // if (isDev) {
+  //   console.log('개발 모드에서는 실제 API 호출을 하지 않습니다.');
+  //   return '개발 모드에서는 실제 API 호출을 하지 않습니다.';
+  // }
 
-  const res = await axios.post('/api/v1/pr/refactor', { diff });
+  const res = await axios.post('/api/v1/pr/refactor', { patch });
   return res.data.choices?.[0]?.message?.content ?? '리팩토링 결과 없음';
 };
